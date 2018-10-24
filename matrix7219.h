6d8e33966585c94f13b8dddc740649b2befe0ba1
shameeth <44321212+shameeth@users.noreply.github.com>
2018-10-24 20:16:27 +0530

    Update matrix7219.h

--- matrix7219.h
+++ matrix7219.h
@@ -1,17 +1,17 @@
-/*
-  Morse.h - Library for flashing Morse code.
-  Created by David A. Mellis, November 2, 2007.
-  Released into the public domain.
-*/
+
 #ifndef matrix7219_h
 #define matrix7219_h
 
-#include "Arduino.h"
+#include <Arduino.h>
+
+
 
 class matrix7219
 {
   public:
-    matrix7219(int DIN_PIN, int CLK_PIN, int CS_PIN int NUM_MAX);
+	
+    matrix7219(int din_pin, int clk_pin, int cs_pin,int max_disp);
+	void Initmatrix();
     void sendCmd(int addr, byte cmd, byte data);
 	void sendCmdAll(byte cmd, byte data);
 	void refresh(int addr);
@@ -19,13 +19,23 @@ class matrix7219
 	void clr();
 	void scrollLeft();
 	void invert();
-	void showDigit(char ch, int col, const uint8_t *data,int dx, int dy)
-	void showChar(char ch, int col, const uint8_t *data);
-	void setCol(int col, byte v);
-	void printChar(unsigned char ch, const uint8_t *data);
-	void scrollChar(unsigned char c, int del);
-	void scrollString(char *s, int del);
+	void showDigit(char ch, int col, const uint8_t *data,int dx, int dy);
+	int showChar(char ch, int col, const uint8_t *data, int dx, int dy);
+	void setCol(int col, byte v,int dx,int dy);
+	int  printChar(unsigned char ch, const uint8_t *data);
+	byte scr[40];
+	 //int i;
+	//byte scr[NUM_MAX*8+8]; // +8 for scrolled char 
+
+	//void scrollChar(unsigned char c, int del);
+	//void scrollString(char *s, int del);
+	
   private:
+	int NUM_MAX ;
+	int DIN_PIN; 
+	int CLK_PIN; 
+	int CS_PIN ;
+
    
 };
 
