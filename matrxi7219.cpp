9913b95711992d4ae1304797daf3f793214840ea
shameeth <44321212+shameeth@users.noreply.github.com>
2018-10-24 20:17:25 +0530

    Update matrxi7219.cpp

--- matrxi7219.cpp
+++ matrxi7219.cpp
@@ -2,9 +2,8 @@
 
 
 #include "matrix7219.h"
-#include <arduino.h>
+// MAX7219 commands:
 
-/ MAX7219 commands:
 #define CMD_NOOP   0
 #define CMD_DIGIT0 1
 #define CMD_DIGIT1 2
@@ -21,22 +20,37 @@
 #define CMD_DISPLAYTEST 15
 
 
-byte scr[NUM_MAX*8 + 8]; // +8 for scrolled char
 
 
-matrix7219::matrxi7219(int DIN_PIN, int CLK_PIN, int CS_PIN, int NUM_MAX)
+matrix7219::matrix7219(int din_pin, int clk_pin , int cs_pin, int max_disp)
 {
+  DIN_PIN = din_pin;
+  CLK_PIN = clk_pin;
+  CS_PIN  = cs_pin;
+  NUM_MAX = max_disp;
   pinMode(DIN_PIN, OUTPUT);
   pinMode(CLK_PIN, OUTPUT);
-  pinMode(CS_PIN, OUTPUT);
+  pinMode(CS_PIN, OUTPUT); 
+ 
+  
+}
+
+
+
+
+void matrix7219::Initmatrix()
+{
   digitalWrite(CS_PIN, HIGH);
-  sendCmdAll(CMD_DISPLAYTEST, 0);
-  sendCmdAll(CMD_SCANLIMIT, 7);
-  sendCmdAll(CMD_DECODEMODE, 0);
-  sendCmdAll(CMD_INTENSITY, 1);
-  sendCmdAll(CMD_SHUTDOWN, 0);
+  sendCmdAll(CMD_DISPLAYTEST, 0);    // dsiaply test OFF
+  sendCmdAll(CMD_SCANLIMIT, 7);      //
+  sendCmdAll(CMD_DECODEMODE, 0);      // decode mode OFF
+  sendCmdAll(CMD_INTENSITY, 0);      
+  sendCmdAll(CMD_SHUTDOWN, 1);     // normal Operation mode
+  //for(i=0; i<NUM_MAX*8+8; i++){
+	  
+  //scr[i];}
   clr();
-  refreshAll();
+  refreshAll();		
 }
 
 void matrix7219::sendCmd(int addr, byte cmd, byte data)
@@ -59,17 +73,22 @@ void matrix7219::sendCmdAll(byte cmd, byte data)
   digitalWrite(CS_PIN, HIGH);
 }
 
-void matrix7219::refresh(int addr) {
+void matrix7219::refresh(int addr) {    
   for (int i = 0; i < 8; i++)
     sendCmd(addr, i + CMD_DIGIT0, scr[addr * 8 + i]);
 }
 
-void matrix7219::refreshAll() {   // write all the scr array cotent into MAX7219 SRAM
+void matrix7219::refreshAll() { 
+//Serial.println("refresh:");       
   for (int c = 0; c < 8; c++) {
     digitalWrite(CS_PIN, LOW);
     for(int i=NUM_MAX-1; i>=0; i--) {
       shiftOut(DIN_PIN, CLK_PIN, MSBFIRST, CMD_DIGIT0 + c);
       shiftOut(DIN_PIN, CLK_PIN, MSBFIRST, scr[i * 8 + c]);
+	 //  Serial.println(i*8+c);
+	 //  Serial.print(":");
+	 //  Serial.print(scr[i * 8 + c],HEX);
+	  // Serial.print(";");
     }
     digitalWrite(CS_PIN, HIGH);
  }
@@ -91,26 +110,35 @@ void matrix7219::invert()
 }
 
 
-void matrix7219::showDigit(char ch, int col, const uint8_t *data)
+void matrix7219::showDigit(char ch, int col, const uint8_t *data, int dx, int dy)
 {
+  // Serial.println("store:");
   if(dy<-8 | dy>8) return;
   int len = pgm_read_byte(data);               //  lenth of character bytes per segment
   int w = pgm_read_byte(data + 1 + ch * len);  //  no of coloumn of segment say 4x8 that is 4
   col += dx;
   for (int i = 0; i < w; i++)                   
     if(col+i>=0 && col+i<NUM_MAX*8) {
-      byte v = pgm_read_byte(data + 1 + ch * len + 1 + i);   // read the byte from the memory and assign to V
+      byte v = pgm_read_byte(data + 1 + ch * len + 1 + i);   // read the byte from the memory and assign to V	 
       if(!dy) scr[col + i] = v; else scr[col + i] |= dy>0 ? v>>dy : v<<-dy;// store the read byte from Falsh memory into Array scr
+	  //Serial.print(scr[col+i],HEX);// no problem in array writing
+	  // Serial.print(col+i);
+	  // Serial.print(":");
+	  // Serial.print(scr[col+i],HEX);
+	   //Serial.print(";");
     }
 }
 
-int matrix7219::showChar(char ch, int col, const uint8_t *data)
+int matrix7219::showChar(char ch, int col, const uint8_t *data,int dx, int dy)
 {
+	//Serial.println(ch,DEC);
+	
+	//ch -= 32;
   int len = pgm_read_byte(data);
-  int i,w = pgm_read_byte(data + 1 + ch * len);
+  int w = pgm_read_byte(data + 1 + ch * len);
   if(dy<-8 | dy>8) return w;
   col += dx;                      // x axis shift
-  for (i = 0; i < w; i++)
+  for (int i = 0; i < w; i++)
     if(col+i>=0 && col+i<NUM_MAX*8) {
       byte v = pgm_read_byte(data + 1 + ch * len + 1 + i);
       if(!dy) scr[col + i] = v; else scr[col + i] |= dy>0 ? v>>dy : v<<-dy;   //y axis shift if dy >0
@@ -119,14 +147,14 @@ int matrix7219::showChar(char ch, int col, const uint8_t *data)
 }
 
 
-void matrix7219::setCol(int col, byte v)
+void matrix7219::setCol(int col, byte v,int dx,int dy)
 {
   if(dy<-8 | dy>8) return;
   col += dx;
   if(col>=0 && col<32)
     if(!dy) scr[col] = v; else scr[col] |= dy>0 ? v>>dy : v<<-dy;
 }
-.
+
 int matrix7219::printChar(unsigned char ch, const uint8_t *data)
 {
   int len = pgm_read_byte(data);
@@ -137,7 +165,7 @@ int matrix7219::printChar(unsigned char ch, const uint8_t *data)
   return w;
 }
 
-void matrix7219::scrollChar(unsigned char c, int del) {
+/*void matrix7219::scrollChar(unsigned char c, int del) {
   c = convertPolish(c);
   if (c < ' ' || c > '~'+22) return;
   c -= 32;
@@ -151,6 +179,8 @@ void matrix7219::scrollChar(unsigned char c, int del) {
 void matrix7219::scrollString(char *s, int del)
 {
   while(*s) scrollChar(*s++, del);
-}
+}*/
+
+
 
 
